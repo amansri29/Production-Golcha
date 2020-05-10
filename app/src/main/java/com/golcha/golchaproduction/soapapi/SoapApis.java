3e51d253204cae97c781804801d229936d99fada
amansricd <amansri29@gmail.com>
2020-05-10 22:16:15 +0530

    Initial Setup Created

---
+++ app/src/main/java/com/golcha/golchaproduction/soapapi/SoapApis.java
@@ -0,0 +1,56 @@
+package com.golcha.golchaproduction.soapapi;
+import android.content.Context;
+import android.util.Log;
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+
+public class SoapApis {
+    private static final String TAG = "SoapApis";
+    public static String userName = "aman.srivastav";
+    public static String password = "Change@123";
+    public static String domain = "gghojai";
+
+    public static void getFgInventoryData()
+    {
+        String namespace2 = "urn:microsoft-dynamics-schemas/page/fgdateandlocationwiserating";
+        String url2 = "http://myerp.golchagroup.com:7048/DynamicsNAV90/WS/UMDS%20Pvt.Ltd./Page/FGDateandLocationWiseRating";
+        String soap_action2 = "urn:microsoft-dynamics-schemas/page/fgdateandlocationwiserating:ReadMultiple";
+        String method_name2 = "ReadMultiple";
+
+        try {
+            SoapObject request = new SoapObject(namespace2, method_name2);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+            envelope.setOutputSoapObject(request);
+
+            NtlmTransport ntlm = new NtlmTransport();
+            ntlm.debug = true;
+            ntlm.setCredentials(url2, userName, password, domain, "");
+
+            ntlm.call(soap_action2, envelope); // Receive Error here!
+
+            try {
+                SoapObject result = (SoapObject) envelope.getResponse();
+                Log.i(TAG, "Aman: " + result);
+                for (int i = 0; i < result.getPropertyCount(); i++) {
+                    SoapObject result2 = (SoapObject) result.getProperty(i);
+                    try {
+
+                        Log.i(TAG, "Response FG Rating " + result2.getProperty("Planned_Delivery_Date") + " " + result2.getProperty("Location_Code") + " " + result2.getProperty("Rating") + " " + result2.getProperty("Overall_Rating"));
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "doInBackground: catch response FG Inventory Data " + e.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            String error = e.toString();
+            Log.i(TAG, "doInBackground: catch " + error);
+        }
+    }
+}
---
+++ app/src/main/java/com/golcha/golchaproduction/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,31 @@
+package com.golcha.golchaproduction.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.golcha.golchaproduction.R;
+import com.golcha.golchaproduction.soapapi.SoapApis;
+
+
+public class DashboardFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+
+//        SoapApis.getFgInventoryData(getActivity());
+
+        return root;
+    }
+}
