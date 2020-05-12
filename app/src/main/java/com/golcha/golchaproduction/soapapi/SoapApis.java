d48bcf577db2f5a0d64d34babab4f758891ab17c
amansricd <amansri29@gmail.com>
2020-05-12 13:36:49 +0530

    product card method added

--- app/src/main/java/com/golcha/golchaproduction/soapapi/SoapApis.java
+++ app/src/main/java/com/golcha/golchaproduction/soapapi/SoapApis.java
@@ -1,6 +1,10 @@
 package com.golcha.golchaproduction.soapapi;
+
 import android.content.Context;
 import android.util.Log;
+
+import com.golcha.golchaproduction.Getplanarraylist;
+
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
@@ -11,8 +15,7 @@ public class SoapApis {
     public static String password = "Change@123";
     public static String domain = "gghojai";
 
-    public static void getFgInventoryData()
-    {
+    public static void getFgInventoryData() {
         String namespace2 = "urn:microsoft-dynamics-schemas/page/fgdateandlocationwiserating";
         String url2 = "http://myerp.golchagroup.com:7048/DynamicsNAV90/WS/UMDS%20Pvt.Ltd./Page/FGDateandLocationWiseRating";
         String method_name2 = "ReadMultiple";
@@ -38,9 +41,7 @@ public class SoapApis {
                     try {
 
                         Log.i(TAG, "Response FG Rating " + result2.getProperty("Planned_Delivery_Date") + " " + result2.getProperty("Location_Code") + " " + result2.getProperty("Rating") + " " + result2.getProperty("Overall_Rating"));
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -55,9 +56,7 @@ public class SoapApis {
     }
 
 
-
-    public static SoapObject getPlannedOrderList()
-    {
+    public static SoapObject getPlannedOrderList() {
         String namespace2 = Urls.planned_production_list_namespace;
         String url2 = Urls.planned_production_list_url;
         String method_name2 = "ReadMultiple";
@@ -85,8 +84,8 @@ public class SoapApis {
         }
         return result;
     }
-    public static SoapObject getUpdatedOrderList()
-    {
+
+    public static SoapObject getUpdatedOrderList() {
         String namespace2 = Urls.updated_production_list_namespace;
         String url2 = Urls.updated_production_list_url;
         String method_name2 = "ReadMultiple";
@@ -114,4 +113,51 @@ public class SoapApis {
         }
         return result;
     }
+
+
+    public static void getPlannedCardDetails(String no) {
+        String namespace2 = Urls.planned_production_card_namespace;
+        String url2 = Urls.planned_production_card_url;
+        String method_name2 = "Read";
+        String soap_action2 = namespace2 + ":" + method_name2;
+
+
+        try {
+            SoapObject request = new SoapObject(namespace2, method_name2);
+            request.addProperty("No", no);
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;
+
+//            SoapObject filter = new SoapObject(namespace2,"filter");
+//            envelope.bodyOut = request;
+            envelope.dotNet = true;
+
+            envelope.setOutputSoapObject(request);
+
+            NtlmTransport ntlm = new NtlmTransport();
+            ntlm.debug = true;
+            ntlm.setCredentials(url2, userName, password, domain, "");
+
+            ntlm.call(soap_action2, envelope); // Receive Error here!
+
+            SoapObject result = (SoapObject) envelope.getResponse();
+
+            try {
+                String no2 = String.valueOf(result.getProperty("No"));
+                String desc1 = String.valueOf(result.getProperty("Description"));
+                String desc2 = String.valueOf(result.getProperty("Description_2"));
+                Log.i(TAG, "Planned Card::" + no2 + " " + desc1 + " " + desc2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            String error = e.toString();
+            Log.e(TAG, "error " + error);
+        }
+//        return result;
+    }
+
 }
--- app/src/main/java/com/golcha/golchaproduction/soapapi/Urls.java
+++ app/src/main/java/com/golcha/golchaproduction/soapapi/Urls.java
@@ -5,8 +5,11 @@ public class Urls {
     private static String company_name = "DummyGST";
 
     public static String planned_production_list_url = base_url +  "/" + company_name +
-            "/WS/UMDS%20Pvt.Ltd./Page/PlannedProductionOrder";
-    public static String planned_production_list_namespace = "urn:microsoft-dynamics-schemas/page/plannedproductionorder";
+            "/WS/UMDS%20Pvt.Ltd./Page/PPOListPage";
+    public static String planned_production_list_namespace = "urn:microsoft-dynamics-schemas/page/ppolistpage";
+
+    public static String planned_production_card_url = "http://myerp.golchagroup.com:1132/DummyGST/WS/UMDS%20Pvt.Ltd./Page/PlannedProdOrder";
+    public static String planned_production_card_namespace = "urn:microsoft-dynamics-schemas/page/plannedprodorder";
 
     public static String updated_production_list_url = base_url +  "/" + company_name +
             "/WS/UMDS%20Pvt.Ltd./Page/rpo";
