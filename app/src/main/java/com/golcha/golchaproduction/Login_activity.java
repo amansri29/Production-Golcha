e2929041ec7b33bc1aab3fedd74b4b9f808ea79e
amansricd <amansri29@gmail.com>
2020-05-15 20:05:02 +0530

    login screen added

--- app/src/main/java/com/golcha/golchaproduction/Login_activity.java
+++ app/src/main/java/com/golcha/golchaproduction/Login_activity.java
@@ -10,6 +10,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,7 +30,10 @@ public class Login_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activity);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
         activity = this;
         list = new ArrayList<>();
         mylist = new ArrayList<>();
